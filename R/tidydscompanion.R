938a443e870a4b74eb84b06e0be53cd72aebce16
Jake Thompson <wjakethompson@gmail.com>
2019-05-10 09:57:44 -0500

    package level documentation

---
+++ R/tidydscompanion.R
@@ -0,0 +1,9 @@
+#' tidydscompanion: A companion package for Using R and the tidyverse for Data
+#' Science
+#'
+#' Provides data sets and installation help for the Using R and the tidyverse
+#' for Data Science workshop
+#'
+#' @docType package
+#' @name tidydscompanion
+NULL
